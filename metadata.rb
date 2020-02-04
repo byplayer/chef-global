0c1d251dcb3735489dd4d29b7d27dc1fe6385eaf
byplayer <byplayer100@gmail.com>
2020-02-04 16:55:00 +0900

    :sparkles: support mac os x
    Support mac os x to install global from source

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 name             'global'
 maintainer       'Yusuke Murata'
 maintainer_email 'info@muratayusuke.com'
 license          'All rights reserved'
 description      'Installs/Configures global'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '1.0.0'
