0f7ab16238ba24a8fe8eb4f519617d357ca13759
byplayer <byplayer100@gmail.com>
2020-02-04 15:31:00 +0900

    :+1: Update default global version.
    Upgrade latest version for default.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
 default['global']['version'] = '6.2.9'
+# frozen_string_literal: true
+
+default['global']['version'] = '6.6.4'
 default['global']['url'] = "http://tamacom.com/global/global-#{node['global']['version']}.tar.gz"
 default['global']['prefix'] = '/usr/local'
