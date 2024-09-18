23b71c3cf2a463f7012fba751b39d0fda4c4f62e
byplayer <byplayer100@gmail.com>
2024-09-18 15:46:59 +0900

    👍 update global download link.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-default['global']['version'] = '6.6.4'
-default['global']['url'] = "http://tamacom.com/global/global-#{node['global']['version']}.tar.gz"
+default['global']['version'] = '6.6.13'
+default['global']['url'] = "https://ftp.gnu.org/pub/gnu/global/global-#{node['global']['version']}.tar.gz"
 default['global']['prefix'] = '/opt/global'
